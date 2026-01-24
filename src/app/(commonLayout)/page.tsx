b5bc024017612ea733ed0ac196a10ed8a64e1c79
Soyab-Aktar <iamsoyabcode23@gmail.com>
2026-01-24 10:12:14 +0530

    Milestone 5, Module 33, Blog App, fetch backend api data, understanding SSR - ISR - SSG, handling params, fetchoptins

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,15 +1,29 @@
+import BlogCard from "@/components/modules/homepage/BlogCard";
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { blogService } from "@/services/blog.service";
 import { userService } from "@/services/user.service";
+import { BlogPost } from "@/types";
 import { cookies } from "next/headers";
 
 export default async function Home() {
-  const { data, error } = await userService.getSession();
+
+
+  const { data } = await blogService.getBlogPost({
+    isFeatured: true,
+    search: "",
+  }, {
+    cache: "no-store"
+  });
+
   console.log(data);
   return (
-    <div className="p-5">
-      <h1 className="text-6xl font-bold"><span className="text-red-500">Home</span> Page</h1>
-      <p><b>Route : </b>/</p>
+    <div className="p-5 grid grid-cols-4 gap-3">
+
+      {data?.data?.data?.map((post: BlogPost) => (
+        <BlogCard key={post.id} post={post} />
+      ))}
+
     </div>
   );
 }
